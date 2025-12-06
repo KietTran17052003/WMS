2312d7030317c79f793c633b0338ad73cd9eea58
Trần Cao Kiệt <97595160+KietTran17052003@users.noreply.github.com>
2025-12-07 04:56:50 +0700

    Delete debug_top_products.php

--- debug_top_products.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/controller/cDashboard.php';
-$cd = new CDashboard();
-$top = $cd->getTopMovingProducts(10);
-echo json_encode($top, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
